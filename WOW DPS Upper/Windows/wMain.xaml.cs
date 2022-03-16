3b30ca6756ab15d44d28bc802bbcfa46b03b9eb5
BriteNight_Cloud <britenight_cloud@mail.ru>
2022-03-16 11:48:28 +0300

    Layout

--- WOW DPS Upper/Windows/wMain.xaml.cs
+++ WOW DPS Upper/Windows/wMain.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WOW_DPS_Upper
+namespace WOW_DPS_Upper.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для wMain.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class wMain : Window
     {
-        public MainWindow()
+        public wMain()
         {
             InitializeComponent();
         }
